f41cb02d83d87530cfc6fc17c592f14abf5b7bc0
Arpan Tolat <Arpan Tolat>
2016-06-30 16:19:27 -0500

    Initialized Repo, added header and template

---
+++ ppmguru-mailing-list.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+Plugin Name: PPM Guru Mailing List
+Description: Custom email list building plugin for WordPress. Capture new subscribers.Build unlimited lists. Import and export subscribers easily with .csv
+Version: 1.0
+Author: Arpan Tolat
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: ppmguru-mailing-list
+*/
+
+/* 
+IMPORTANT: Snappy List Builder is now a plugin in the WordPress plugin directory. This means, that if you name your file snappy-list-builder.php you will be prompted in WordPress to update your plugin as WordPress will treat it like the production version of Snappy List Builder.
+*/
+
+/*
+THE FIX: Simply rename your plugin directory to "my-snappy-list-builder" and you php file to my-snappy-list-builder.php and update your plugin header data Text Domain to "my-snappy-list-builder". Save your .php file and you should be good to go!	
+*/
+	
+/* !0. TABLE OF CONTENTS */
+
+/*
+	
+	1. HOOKS
+	
+	2. SHORTCODES
+		
+	3. FILTERS
+		
+	4. EXTERNAL SCRIPTS
+		
+	5. ACTIONS
+		
+	6. HELPERS
+		
+	7. CUSTOM POST TYPES
+	
+	8. ADMIN PAGES
+	
+	9. SETTINGS
+	
+	10. MISCELLANEOUS 
+
+*/
+
+
+
+
+/* !1. HOOKS */
+
+
+
+
+/* !2. SHORTCODES */
+
+
+
+
+/* !3. FILTERS */
+
+
+
+
+/* !4. EXTERNAL SCRIPTS */
+
+
+
+
+/* !5. ACTIONS */
+
+
+
+
+/* !6. HELPERS */
+
+
+
+
+/* !7. CUSTOM POST TYPES */
+
+
+
+
+/* !8. ADMIN PAGES */
+
+
+
+
+/* !9. SETTINGS */
+
+
+
+
+/* !10. MISCELLANEOUS */
+
+
+
