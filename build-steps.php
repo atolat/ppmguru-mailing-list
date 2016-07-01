d11738f111b36dbc95fc36eb709cb5fd827bc1be
Arpan Tolat <Arpan Tolat>
2016-07-01 10:52:05 -0500

    Added filters and hooks for custom fields

---
+++ build-steps.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+
+Build Steps
+
+1. Added a shortcode to generate a subscription form.
+
+2. Added a function to register shortcodes.
+
+3. Added a hook to add shortcodes on init.
+
+4. Built custom post types using CPTUI for subscribers and lists.
+
+5. Built custom fields for Subscribers post types using ACFUI.
+
+6. Added functions to configure custom headers and custom data for the lists and subscribers post types.
+
+*/
+
+
+?>
--- ppmguru-mailing-list.php
+++ ppmguru-mailing-list.php
@@ -15,13 +15,20 @@ Text Domain: ppmguru-mailing-list
 /*
 	
 	1. HOOKS
-        1.1 add_action()
+        1.1 register shortcodes on init
+        1.2 add filter to register custom admin column headers
+        1.3 add filter to register custom column data
 	
 	2. SHORTCODES
         2.1 pgm_register_shortcodes()
         2.2 pgm_form_shortcode()
 		
 	3. FILTERS
+        3.1 pgm_subscriber_column_headers($columns)
+        3.2 pgm_subscriber_column_data($columns,$post_id)
+        3.3 pgm_register_custom_admin_titles()
+        3.4 pgm_custom_admin_titles( $title, $post_id )
+        3.5 pgm_list_column_headers($columns)
 		
 	4. EXTERNAL SCRIPTS
 		
@@ -43,7 +50,16 @@ Text Domain: ppmguru-mailing-list
 
 
 /* !1. HOOKS */
+//1.1 register shortcodes on init
 add_action('init','pgm_register_shortcodes');
+
+//1.2 add filter to register custom admin column headers
+add_filter('manage_edit-pgm_subscriber_columns','pgm_subscriber_column_headers');
+add_filter('manage_edit-pgm_list_columns','pgm_list_column_headers');
+
+//1.3 add filter to register custom column data
+add_filter('manage_pgm_subscriber_posts_custom_column','pgm_subscriber_column_data',1,2);
+add_action('admin_head-edit.php','pgm_register_custom_admin_titles');
 
 
 
@@ -110,8 +126,72 @@ return $output;
 
 
 /* !3. FILTERS */
+//3.1 This function will take in the admin colums from the subscriber post page and override the column names.
+function pgm_subscriber_column_headers($columns){
+    $columns = array(
+    'cb'=>'<input type="checkbox" />', //checkbox-HTML empty checkbox
+    'title'=>__('Subscriber Name'), //update header name to 'Subscriber Name'
+    'email'=>__('Email Address'), //create new header for email
+    );
+    
+    return $columns;
+}
 
 
+//3.2 This function will take in the column data, check the header for title or email and get the corresponding data, append it to the output stream and echo it. 
+function pgm_subscriber_column_data($column,$post_id){
+    $output = '';
+    
+    switch($column){
+            
+        case 'title':
+            $fname = get_field('pgm_fname', $post_id);
+            $lname = get_field('pgm_lname', $post_id);
+            $output .= $fname.' '.$lname;
+            break;
+        case 'email':
+            $email = get_field('pgm_email', $post_id);
+            $output .= $email;
+            break;
+    }
+    
+    echo $output;
+}
+
+//3.3 registers special custom admin title columns- fix for newer version of wordpress
+function pgm_register_custom_admin_titles() {
+    add_filter('the_title','pgm_custom_admin_titles',99,2);
+}
+
+//3.4 handles custom admin title "title" column data for post types without titles, we are not using wordpress default titles in our custom field.
+function pgm_custom_admin_titles( $title, $post_id ) {
+   
+    global $post;
+	
+    $output = $title;
+   
+    if( isset($post->post_type) ):
+                switch( $post->post_type ) {
+                        case 'pgm_subscriber':
+	                            $fname = get_field('pgm_fname', $post_id );
+	                            $lname = get_field('pgm_lname', $post_id );
+	                            $output = $fname .' '. $lname;
+	                            break;
+                }
+        endif;
+   
+    return $output;
+}
+
+//3.5 function to handle list post headers
+function pgm_list_column_headers($columns){
+    $columns = array(
+    'cb'=>'<input type="checkbox" />', //checkbox-HTML empty checkbox
+    'title'=>__('Lists'), //update header name to 'List Name'
+    );
+    
+    return $columns;
+}
 
 
 /* !4. EXTERNAL SCRIPTS */
